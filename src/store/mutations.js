cc8343e7ef2be1bcdb7d038d59fe0a0d2dad372d
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-16 10:08:40 +0700

    reconfigured vuex of project

---
+++ src/store/mutations.js
@@ -0,0 +1,10 @@
+// https://vuex.vuejs.org/en/mutations.html
+
+export default {
+  SET_BAR_IMAGE (state, payload) {
+    state.barImage = payload
+  },
+  SET_DRAWER (state, payload) {
+    state.drawer = payload
+  },
+}
