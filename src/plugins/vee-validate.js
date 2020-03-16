5b9654284509e83b1c123abbb31b305626622e12
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-16 11:30:04 +0700

    configured vee-validate

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -4,19 +4,16 @@ import {
   ValidationObserver,
   ValidationProvider,
 } from 'vee-validate'
-import {
-  email,
-  max,
-  min,
-  numeric,
-  required,
-} from 'vee-validate/dist/rules'
 
-extend('email', email)
-extend('max', max)
-extend('min', min)
-extend('numeric', numeric)
-extend('required', required)
+import * as rules from 'vee-validate/dist/rules'
+import { messages } from 'vee-validate/dist/locale/en.json'
+
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+    message: messages[rule], // assign message
+  })
+})
 
 Vue.component('validation-provider', ValidationProvider)
 Vue.component('validation-observer', ValidationObserver)
