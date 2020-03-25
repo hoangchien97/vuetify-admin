5d8a74c2a20cab0e47129c7f7a45d54e7b38066c
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-25 14:53:10 +0700

    update daily log work

--- src/views/DailyLogWork.vue
+++ src/views/DailyLogWork.vue
@@ -14,66 +14,6 @@
       <v-row justify="center">
         <v-col cols="12" lg="2">
           <v-menu
-            v-model="menu3"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="dayLogWork"
-                label="Day Working Log"
-                append-icon="mdi-calendar"
-                outlined
-                readonly
-                @blur="formData.startDateCreated = parseDate(dayLogWork)"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="formData.startDateCreated"
-              @input="menu3 = false"
-            ></v-date-picker>
-          </v-menu>
-          <p>
-            Day Working Log: <strong>{{ formData.startDateCreated }}</strong>
-          </p>
-        </v-col>
-
-        <v-col cols="12" lg="2">
-          <v-menu
-            v-model="menu4"
-            :close-on-content-click="false"
-            :nudge-right="40"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="endDayLogWork"
-                label="Day Working Log"
-                append-icon="mdi-calendar"
-                outlined
-                readonly
-                @blur="formData.endDateCreated = parseDate(endDayLogWork)"
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="formData.endDateCreated"
-              @input="menu4 = false"
-            ></v-date-picker>
-          </v-menu>
-          <p>
-            End day Working Log: <strong>{{ formData.endDateCreated }}</strong>
-          </p>
-        </v-col>
-
-        <v-col cols="12" lg="2">
-          <v-menu
             v-model="menu1"
             :close-on-content-click="false"
             :nudge-right="40"
@@ -95,11 +35,11 @@
             <v-date-picker
               v-model="formData.startDate"
               :max="formData.endDate"
-              @input="menu1 = false"
+              @input="selectStartDate()"
             ></v-date-picker>
           </v-menu>
           <p>
-            Start Date: <strong>{{ formData.startDate }}</strong>
+            <!-- Start Date: <strong>{{ formData.startDate }}</strong> -->
           </p>
         </v-col>
         <v-col cols="12" lg="2">
@@ -113,6 +53,7 @@
           >
             <template v-slot:activator="{ on }">
               <v-text-field
+                :ref="'end-day-log-work'"
                 v-model="endDateFormatted"
                 label="End Date"
                 append-icon="mdi-calendar"
@@ -126,13 +67,74 @@
               v-model="formData.endDate"
               :min="formData.startDate"
               :max="dayNow"
-              @input="menu2 = false"
+              @input="selectEndDate()"
             ></v-date-picker>
           </v-menu>
           <p>
-            End Date: <strong>{{ formData.endDate }}</strong>
+            <!-- End Date: <strong>{{ formData.endDate }}</strong> -->
           </p>
         </v-col>
+
+        <v-col cols="12" lg="2">
+          <v-menu
+            v-model="menu3"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="startDateSync"
+                label="Start Sync Date"
+                append-icon="mdi-calendar"
+                outlined
+                readonly
+                @blur="formData.startDateCreated = parseDate(startDateSync)"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="formData.startDateCreated"
+              @input="selectStartSyncDate()"
+            ></v-date-picker>
+          </v-menu>
+          <p>
+            <!-- Start Sync Date: <strong>{{ formData.startDateCreated }}</strong> -->
+          </p>
+        </v-col>
+
+        <v-col cols="12" lg="2">
+          <v-menu
+            v-model="menu4"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="endDateSync"
+                label="End Sync Date"
+                append-icon="mdi-calendar"
+                outlined
+                readonly
+                @blur="formData.endDateCreated = parseDate(endDateSync)"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="formData.endDateCreated"
+              @input="selectEndSyncDate()"
+            ></v-date-picker>
+          </v-menu>
+          <p>
+            <!-- End Sync Date: <strong>{{ formData.endDateCreated }}</strong> -->
+          </p>
+        </v-col>
+
         <v-col cols="12" lg="2">
           <!-- https://vuetifyjs.com/en/components/selects/ -->
           <v-select
@@ -144,11 +146,13 @@
             label="Project Jira"
             persistent-hint
             single-line
+            clearable
+            @input="search()"
           ></v-select>
         </v-col>
-        <v-col cols="12" lg="3">
+        <v-col cols="12" lg="2">
           <v-text-field
-            v-model="formData.txtSearch"
+            v-model="txtSearch"
             name="Search"
             :label="$t('search')"
             hide-details
@@ -163,13 +167,42 @@
           </v-text-field>
         </v-col>
       </v-row>
-      <v-row> </v-row>
+      <template v-if="listLogSync">
+        <v-row>
+          <v-col cols="12">
+            <v-data-table
+              :headers="headers"
+              :items="filteredItems"
+              :search="txtSearch"
+              no-results-text="No results"
+              no-data-text="No results"
+              sort-by="id"
+              class="elevation-1"
+            >
+              <!-- <template v-slot:item.stt="">
+                {{ ++stt }}
+              </template> -->
+              <template v-slot:item.date="{ item }">
+                {{ convertDate(item.date) }}
+              </template>
+              <template v-slot:item.createdAt="{ item }">
+                {{ convertDate(item.createdAt) }}
+              </template>
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </template>
+      <template v-else>
+        <div class="text-center">
+          No data to show
+        </div>
+      </template>
     </base-material-card>
   </v-container>
 </template>
 
 <script>
-// import debounce from "lodash/debounce"
+import debounce from "lodash/debounce"
 import moment from "moment"
 export default {
   name: "DailyLogWork",
@@ -177,18 +210,18 @@ export default {
     return {
       formData: {
         // startDate: "2020-03-20" - ngay lam viec // working-day
-        // startDateCreated: "2020-03-28" - ngay log work ( log muon ) // day-working-log
         // endDate : get data tu ngay lam viec den hom nay
-        // endDateCreated: ngay log work tu ngay nao den ngay nao
+        // startDateCreated: "2020-03-28" - ngay sync
+        // endDateCreated: ngay ket thuc sync
         startDate: moment()
           .subtract(1, "days")
           .format("YYYY-MM-DD"),
         endDate: moment().format("YYYY-MM-DD"),
         startDateCreated: "",
         endDateCreated: "",
-        txtSearch: "",
-        jira_project: "",
+        jira_project: null,
       },
+      txtSearch: "",
       dayNow: moment().format("YYYY-MM-DD"),
       startDateFormatted: this.formatDate(
         moment()
@@ -196,12 +229,29 @@ export default {
           .format("YYYY-MM-DD")
       ),
       endDateFormatted: moment().format("DD/MM/YYYY"),
-      dayLogWork: "",
-      endDayLogWork: "",
+      startDateSync: "",
+      endDateSync: "",
       menu1: false,
       menu2: false,
       menu3: false,
       menu4: false,
+      stt: 0,
+      headers: [
+        { text: "Work Date", value: "date" },
+        { text: "Sync Date", value: "createdAt" },
+        { text: "Username Jira", value: "jira_username" },
+        { text: "Project Jira", value: "jira_project" },
+        {
+          text: "Username Timesheet",
+          sortable: true,
+          align: "start",
+          filterable: true,
+          value: "svc_userid",
+        },
+        { text: "Project Timesheet", value: "svc_project" },
+        { text: "Working Time", value: "workingtime" },
+        { text: "Status", value: "description" },
+      ],
     }
   },
 
@@ -212,10 +262,10 @@ export default {
     computedEndDateFormatted() {
       return this.formatDate(this.formData.endDate)
     },
-    dayLogWorkFormatted() {
+    startSyncDate() {
       return this.formatDate(this.formData.startDateCreated)
     },
-    endDayLogWorkFormatted() {
+    endSyncDate() {
       return this.formatDate(this.formData.endDateCreated)
     },
     listProjectJira() {
@@ -223,6 +273,14 @@ export default {
     },
     listLogSync() {
       return this.$store.state.listLogSync
+    },
+    filteredItems() {
+      return this.listLogSync.filter(i => {
+        return (
+          !this.formData.jira_project ||
+          i.jira_project === this.formData.jira_project
+        )
+      })
     },
   },
   watch: {
@@ -233,10 +291,16 @@ export default {
       this.endDateFormatted = this.formatDate(this.formData.endDate)
     },
     "formData.startDateCreated"(val) {
-      this.dayLogWork = this.formatDate(this.formData.startDateCreated)
+      this.startDateSync = this.formatDate(this.formData.startDateCreated)
     },
     "formData.endDateCreated"(val) {
-      this.endDayLogWork = this.formatDate(this.formData.endDateCreated)
+      this.endDateSync = this.formatDate(this.formData.endDateCreated)
+    },
+    "formData.jira_project": {
+      handler(val) {
+        console.log(val)
+      },
+      deep: true,
     },
   },
 
@@ -246,6 +310,10 @@ export default {
   },
 
   methods: {
+    convertDate(date) {
+      return moment(date).format("DD-MM-YYYY")
+    },
+
     formatDate(date) {
       if (!date) return null
       const [year, month, day] = date.split("-")
@@ -259,11 +327,51 @@ export default {
     async getProjectJira() {
       await this.$store.dispatch("getListProjectJira")
     },
-    async getLogSync(data) {
-      await this.$store.dispatch("getLogSync", data)
+    async getLogSync() {
+      await this.$store.dispatch("getLogSync", { ...this.formData })
     },
+
+    selectStartDate() {
+      this.menu1 = false
+      this.focusInput("end-day-log-work")
+      this.startDateFormatted = this.formatDate(this.formData.startDate)
+      this.getLogSync({ ...this.formData })
+    },
+
+    selectEndDate() {
+      this.menu2 = false
+      this.endDateFormatted = this.formatDate(this.formData.endDate)
+      this.getLogSync({ ...this.formData })
+    },
+
+    selectStartSyncDate() {
+      this.menu3 = false
+      this.endDateFormatted = this.formatDate(this.formData.endDate)
+      if (this.formData.endDateCreated) {
+        this.getLogSync({ ...this.formData })
+      }
+    },
+
+    selectEndSyncDate() {
+      this.menu4 = false
+      this.endDateFormatted = this.formatDate(this.formData.endDate)
+      if (this.formData.startDateCreated) {
+        this.getLogSync({ ...this.formData })
+      }
+    },
+
+    focusInput(nameRef) {
+      this.$nextTick(() => {
+        console.log(11)
+        this.$refs[nameRef].focus()
+      })
+    },
+    search: debounce(async function() {
+      console.log({ ...this.formData })
+      await this.getLogSync()
+    }, 1000),
   },
 }
 </script>
 
-<style></style>
+<style lang="scss"></style>
